3fd913d7ed70d15ef80c460dd2021d2ecff3bef4
Martín Anzures <Martin.Anzures@faithlife.com>
2022-09-07 20:05:53 -0500

    Primera entrega

--- js/main.js
+++ js/main.js
@@ -1,39 +1,124 @@
-alert("Bienvenido a la calculadora del índice de masa corporal. \nSe te pedirá que llenes varios campos para calcularlo.")
+alert("Bienvenido al proyecto. \nSe te pedirá que contestes varias preguntas y se te dará el resultado al final.");
 
-let calculateIMC = (height, weight) => {
-    let imc = (weight / ((height * height) / 10000)).toFixed(2);
-    return imc;
-}
+const questionsList = [
+    {
+        question: "¿Cómo se denomina el lenguaje de marcas de hipertexto?",
+        a: "HTML",
+        b: "PHP",
+        c: "CSS",
+        d: "Javascript",
+        correct: "a",
+    },
+    {
+        question: "¿Cuál de estas etiquetas permite hacer saltos de línea?",
+        a: "<br>",
+        b: "<table>",
+        c: "<body>",
+        d: "<font size>",
+        correct: "a",
+    },
+    {
+        question: "HTML se conoce como el lenguaje de marcación porque los autores insertan instrucciones especiales llamadas _____ HTML, que especifican cómo debe aparecer un documento cuando se despliega en la pantalla de un ordenador o se imprime",
+        a: "echo",
+        b: "URL",
+        c: "etiquetas",
+        d: "mandatos",
+        correct: "c",
+    },
+    {
+        question: "¿Qué está mal en esta regla de estilo?: .cuadro { border: 1px blue dotted padding: 10px 5px; }",
+        a: "Falta un ';' (punto y coma) al final de la declaración del estilo 'border'",
+        b: "Falta una ',' (coma) entre los dos valores de la propiedad padding (10px 5px)",
+        c: "No se puede poner un '.' (punto) al inicio de una declaración (antes de la palabra 'cuadro)'",
+        d: "En lugar de ':' se tiene que utilizar '\"' ",
+        correct: "a",
+    },
+    {
+        question: "La etiqueta HTML <img src> se utiliza para hacer referencia a una ____ que aparecerá en la página web",
+        a: "Lista",
+        b: "Tabla",
+        c: "Imagen",
+        d: "Foto",
+        correct: "c",
+    },
+    {
+        question: "Es un protocolo que funciona en conjunto con TCP/IP para llevar recursos web al escritorio",
+        a: "HTTP",
+        b: "Navegador",
+        c: "www",
+        d: "Buscador",
+        correct: "a",
+    },
+    {
+        question: "¿Cómo se incluye un comentario nuestro en una página HTML para que no se interprete por el navegador?",
+        a: "Con 3 asteriscos al principio y final del comentario. Ej: ***comentario sobre el código***",
+        b: "Con 3 barras al principio y final del comentario. Ej: ///comentario sobre el código///",
+        c: "Con la etiqueta de apertura ' al principio y final del comentario. Ej: <!--comentario sobre el código-->",
+        d: "Con 2 barras al principio del comentaria Ej; //comentario sobre el código",
+        correct: "c",
+    },
+    {
+        question: "¿Por qué es conveniente incluir la declaración de DOCTYPE en la página HTML?",
+        a: "Para evitar que el navegador entre en 'Quirk Mode' y la interprete/muestre mal",
+        b: "Para indicar si se ha utilizado o no Flash",
+        c: "Si no se incluye el navegador no puede renderizar (mostrar) la página",
+        d: "Para cargar los estilos",
+        correct: "a",
+    },
+    {
+        question: "¿Cuál crees que es la mejor manera de aplicar estilos a una página web?",
+        a: "Incluir los estilos en las etiquetas HTML para que se carguen y ejecuten antes",
+        b: "Incluirlos en un fichero externo vinculado a ese fichero HTML",
+        c: "Incluirlos en la sección cabecera ('head') para agruparlos en unmismo sitio en la misma página",
+        d: "Incluirlos al final del body",
+        correct: "b",
+    },
+    {
+        question: "¿Cuál es el lenguaje estándar específico para aplicar estilos de presentación a nuestras páginas web?",
+        a: "CSS",
+        b: "Javascript",
+        c: "Flash",
+        d: "HTML",
+        correct: "a",
+    },
+    {
+        question: "¿Cuál es la etiqueta que se utiliza para poner el texto en cursiva?",
+        a: "<font>",
+        b: "<p>",
+        c: "<i>",
+        d: "<m>",
+        correct: "c",
+    },
+]
 
-const complexionArray = [{name: "Insuficiencia ponderal", min: 0, max: 18.5}, {name: "normal", min: 18.5, max: 25}, {name: "Sobrepeso", min: 25, max: 30}, {name: "Obesidad", min: 30, max: Infinity},]
-
-let evaluateObj = (imc, obj) => {
-    if (imc >= obj.min && imc < obj.max) {
-        return obj.name;
-    }else{
-        return null;
+let verifyAnswer = (response, correct) => {
+    if (response === correct) {
+        return true;
+    } else {
+        return false;
     }
 }
 
-let evaluateArray = (imc, array) => {
-    for (let index = 0; index < array.length; index++) {
-        const element = array[index];
-        let eval = evaluateObj(imc, element);
-        if (eval) {
-            return eval;
-        }
+let evaluatePerformance = (score) => {
+    if (score == 11) {
+        return "Excelente resultado!"
+    } else if (score > 8) {
+        return "Tuviste un buen desempeño pero puedes mejorar"
+    } else {
+        return "Necesitas estudiar"
     }
 }
-let height = Number(prompt("¿Cuál es tu estatura? (cm)"));
-let weight = Number(prompt("¿Cuánto pesas (kg)?"));
 
-let result = calculateIMC(height, weight);
-let complexion = evaluateArray(result, complexionArray);
+let score = 0;
 
-alert("Tu Índice de Masa Corporal es: " + result + "\nTu complexión es: " + complexion);
+questionsList.forEach((element, i) => {
+    let question = prompt((i+1) + ". " + element.question + "\na: " + element.a + "\nb: " + element.b + "\nc: " + element.c + "\nd: " + element.d);
+    if(verifyAnswer(question, element.correct)){
+        score ++;
+    }
+});
 
-
-
+alert("Tuviste " + score + " preguntas correctas de " + questionsList.length + "\n" + evaluatePerformance(score));
 
 
 
